e95dc7167d2726c381d9d8b140b19b20e56c4d26
박세진 <jinpark99@naver.com>
2023-11-12 04:13:09 +0900

    fix: Message record->class 변경

--- chat_server/src/main/java/com/chat/Message.java
+++ chat_server/src/main/java/com/chat/Message.java
@@ -1,4 +1,28 @@
 package com.chat;
 
-public record Message(String content) {}
+public class Message {
+
+    private String type;
+    private String sender;
+    private String message;
+
+    public Message(String type, String sender, String message) {
+        this.type = type;
+        this.sender = sender;
+        this.message = message;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public String getSender() {
+        return sender;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+}
 

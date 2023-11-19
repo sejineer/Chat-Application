eb327c4084d824204732fe126b6d43f114e54430
Park Sejin <jinpark99@naver.com>
2023-11-19 17:50:03 +0900

    feat: chat 기능 구현

--- chat_server/src/main/java/com/chat/handler/CSChatHandler.java
+++ chat_server/src/main/java/com/chat/handler/CSChatHandler.java
@@ -1,12 +1,44 @@
 package com.chat.handler;
 
+import com.chat.client.Client;
+import com.chat.message.JsonMessageFormatter;
 import com.chat.message.Message;
+import com.chat.message.MessageSender;
+import com.google.gson.JsonObject;
 
 public class CSChatHandler implements MessageHandler {
 
+    private MessageSender messageSender;
+
+    public CSChatHandler(MessageSender messageSender) {
+        this.messageSender = new MessageSender(new JsonMessageFormatter());
+    }
+
     @Override
     public void handle(Message message) {
+        JsonObject jsonData = message.getJsonData();
+        String chat = jsonData.get("text").getAsString();
 
+        Client client = message.getClient();
+        if (client.getCurrentRoom() != null) {
+            createChatResponseMessage(client, chat);
+            for (Client roomMember : client.getCurrentRoom().getMembers()) {
+                createChatResponseMessage(roomMember, chat);
+            }
+        } else {
+            JsonObject responseMessage = new JsonObject();
+            responseMessage.addProperty("type", "SCSystemMessage");
+            responseMessage.addProperty("text", "현재 대화방에 들어가 있지 않습니다.");
+            messageSender.sendMessage(client.getChannel(), responseMessage);
+        }
+    }
+
+    private void createChatResponseMessage(Client client, String text) {
+        JsonObject responseMessage = new JsonObject();
+        responseMessage.addProperty("type", "SCChat");
+        responseMessage.addProperty("member", client.getName());
+        responseMessage.addProperty("text", text);
+        messageSender.sendMessage(client.getChannel(), responseMessage);
     }
 
 }
